865e36dbfe9ef03404bc7efc729fb68b6ec2fa99
Veronica Pichay <veronicapichay@outlook.com>
2022-08-02 20:47:56 -0700

    Created Services

---
+++ PartyUsher.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+namespace PartyUsher.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+
+    AuthenticationResponse Login(LoginRequest request);
+
+    AuthenticationResponse Register(RegisterRequest request);
+
+
+
+
+}
