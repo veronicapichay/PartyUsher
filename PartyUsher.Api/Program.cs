16c825aac1e1129cd37200401776b30a45b29bd2
Veronica Pichay <veronicapichay@outlook.com>
2022-08-09 21:02:24 -0700

    Registered Services

--- PartyUsher.Api/Program.cs
+++ PartyUsher.Api/Program.cs
@@ -1,6 +1,7 @@
+using PartyUsher.Application.Services.Authentication;
 var builder = WebApplication.CreateBuilder(args);
 {
-
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
     builder.Services.AddControllers(); //use it for configuration and dependency injection 
 
 }
